436db7bb9b6bc2677d60fa4195e6fcff535d9b20
Leo Kraft <56594417+leokraft@users.noreply.github.com>
2021-06-09 01:43:09 +0200

    Some first lines to get started.

---
+++ src/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <windows.h>
+
+int main() {
+    // NULL -> messages are posted to the message queue of the calling thread and must be processed in the message loop
+    // Virtual-Key Codes -> https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+    bool success = RegisterHotKey(NULL, 1, MOD_CONTROL, 0x59);
+
+    /**
+     * TODOS
+     * 
+     * Function to bind hotkey 
+     * Unbind hotkey on exit
+     * 
+     * Simple window using Qt?
+     * Hotkey input in window
+     * 
+     * open window on hotkey
+     * close window on focus loss (?)
+     * 
+     * run app in tray/background
+     * add app to autostart
+     * 
+     * get list of special characters
+     * copy character into clipboard
+     * remember last focus???
+     * return to last focus?
+     */
+    if (success) {
+        
+        MSG msg = {0};
+        bool flag = false;
+        while (!flag && GetMessage(&msg, NULL, 0, 0) != 0) {
+            if (msg.message == WM_HOTKEY) {
+                std::cout << "Wild hotkey found!" << std::endl;
+                flag = true;   
+            };
+        };
+
+        if (UnregisterHotKey(NULL, 1)) {
+            std::cout << "Unregister successful." << std::endl;
+        };
+
+        return 0;
+    };
+
+    std::cout << "Failed to setup hotkey.\nExiting..." << std::endl;
+    return 1;
+    
+};
+
+/* FOCUS
+ When the keyboard focus changes from one window to another,
+ the system sends a WM_KILLFOCUS message to the window that has lost the focus,
+ and then sends a WM_SETFOCUS message to the window that has gained the focus.
+
+ The user can activate a top-level window by clicking it,
+ selecting it using the ALT+TAB or ALT+ESC key combination,
+ or selecting it from the Task List.
+ A thread can activate a top-level window by using the SetActiveWindow function.
+ It can determine whether a top-level window it created is active by using the GetActiveWindow function.
+
+
+
+ Applications can use a hot key control to make it easy for the user to choose a hot key. 
+ Hot key controls are typically used to define a hot key that activates a window;
+ they do not use the RegisterHotKey and UnregisterHotKey functions.
+ Instead, an application that uses a hot key control typically sends the WM_SETHOTKEY message to set the hot key.
+ Whenever the user presses the hot key, the system sends a WM_SYSCOMMAND message specifying SC_HOTKEY.
+ For more information about hot key controls, see "Using Hot Key Controls" in Hot Key Controls.
+*/
