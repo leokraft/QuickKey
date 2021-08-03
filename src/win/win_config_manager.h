9eafe40c8d95f954647d5eb0e335b302fbbf405f
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-08-03 21:21:45 +0200

    Use new appdata config

--- src/win/win_config_manager.h
+++ src/win/win_config_manager.h
@@ -3,13 +3,22 @@
 
 #include <string>
 #include "i_config_manager.h"
+#include "win_path_manager.h"
+#include <iostream>
+#include <fstream>
+
 
 class WinConfigManager : public IConfigManager {
     public:
-        WinConfigManager(){}
+        WinConfigManager();
         ~WinConfigManager(){}
         void writeConfig(const std::string key, const std::string value);
         std::string getConfig(const std::string key);
+
+    private:
+        void moveConfig();
+        WinPathManager pathManager;
+        std::string appdataPath = pathManager.configPath;
 };
 
 #endif
